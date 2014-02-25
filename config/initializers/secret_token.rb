7c97101553fd174f2240265c4aa2e5d4577267f9
Rodrigo Soares <RodrigoSoares@Rodrigos-MacBook-Air.local>
2014-02-25 16:47:21 -0300

    Restarting project. Installed rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportcircle::Application.config.secret_key_base = 'd6d4cb38995b0f7091b700e5c3ba73a9010e51cbea221907f159e739d78fd1853e0a3025d0c34937c4f404165724229b7f34bfb10269e69da58994d2230f54c3'
