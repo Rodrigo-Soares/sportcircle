0ebc3ff3fa8fd6dc3413fcd65885b159e743ad62
Rodrigo Soares <roderswiss@outlook.com>
2014-02-27 16:44:45 -0300

    Added a bunch of tests for custom_test_test

---
+++ test/integration/custom_test_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class CustomTestTest < ActionDispatch::IntegrationTest
+  
+  test 'The /login direcotry is successfully redirecting to the sign in page' do
+  	get '/login'
+
+  	assert_response :success
+  	
+  end
+
+  test 'The /register direcotry is successfully redirecting to the sign up page' do
+  	get '/register'
+
+  	assert_response :success
+  	
+  end
+
+test 'The /logout direcotry is successfully redirecting to the logout page' do
+  	get '/logout'
+
+  	assert_response :redirect
+  	assert_redirected_to '/'
+
+  	
+  end
+test 'The /edit direcotry is successfully redirecting to the edit page' do
+  	get '/login'
+
+  	assert_response :success
+  	
+  end
+
+
+
+end
